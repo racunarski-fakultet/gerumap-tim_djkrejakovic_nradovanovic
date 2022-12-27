5605bcc6e0c8bef6517a1764526cf584e72dc325
StudentRAF <nradovanovic12022rn@raf.rs>
2022-12-27 23:37:15 +0100

    Release Beta Version: 0.1.0-3

--- src/main/java/rs/raf/gerumap/gui/swing/SwingGui.java
+++ src/main/java/rs/raf/gerumap/gui/swing/SwingGui.java
@@ -2,15 +2,12 @@ package rs.raf.gerumap.gui.swing;
 
 import rs.raf.gerumap.gui.Gui;
 import rs.raf.gerumap.gui.swing.controller.UIManager;
-import rs.raf.gerumap.gui.swing.util.PreferenceUtils;
 import rs.raf.gerumap.gui.swing.view.MainWindow;
 
 public class SwingGui implements Gui {
 
     @Override
     public void start() {
-        PreferenceUtils.clear(); //TODO Remove on release
-
         UIManager.setup();
 
         MainWindow.window.setVisible(true);
